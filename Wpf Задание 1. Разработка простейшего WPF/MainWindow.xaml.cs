29f34fb34f5beb21c92e3f4f0f115d46668a22b6
Максим Алехин <Max-crf@mail.ru>
2022-02-08 15:32:12 +0700

    Добавьте файлы проекта.

---
+++ Wpf Задание 1. Разработка простейшего WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Задание_1.Разработка_простейшего_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, DragEventArgs e)
+        {
+            InitializeComponent();
+            textBlock.Text = 1234.567.ToString("N2");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
